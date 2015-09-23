194af62a1e38f0fc78017bd51a194bb6d2d76c56
Kazuki Yamaguchi <k@rhe.jp>
2015-09-23 18:11:46 +0900

    にゃにゃ

--- config/conf/login.lua
+++ config/conf/login.lua
@@ -34,13 +34,13 @@ end
 local bans = redis:zscore("bans", ngx.var.remote_addr)
 local locks = redis:zscore("locks", login)
 
-print bans
+print(bans)
 if bans ~= ngx.null and bans >= 10 then
   login_fail()
   return ngx.redirect("/?notice=You%27re+banned.")
 end
 
-print locks
+print(locks)
 if locks ~= ngx.null and locks >= 3 then
   login_fail()
   return ngx.redirect("/?notice=This+account+is+locked.")
