b93b3a03810813da747c186a579e6c44d53827c5
Kazuki Yamaguchi <k@rhe.jp>
2015-09-23 15:45:56 +0900

    add mypage.lua

---
+++ config/conf/mypage.lua
@@ -0,0 +1,74 @@
+if not ngx.var.cookie_user_id then
+  ngx.redirect "/?notice=You+must+be+logged+in"
+end
+
+
+ngx.print [[
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="/stylesheets/bootstrap.min.css">
+    <link rel="stylesheet" href="/stylesheets/bootflat.min.css">
+    <link rel="stylesheet" href="/stylesheets/isucon-bank.css">
+    <title>isucon4</title>
+  </head>
+  <body>
+    <div class="container">
+      <h1 id="topbar">
+        <a href="/"><img src="/images/isucon-bank.png" alt="いすこん銀行 オンラインバンキングサービス"></a>
+      </h1>
+<div class="alert alert-success" role="alert">
+  ログインに成功しました。<br>
+  未読のお知らせが０件、残っています。
+</div>
+
+<dl class="dl-horizontal">
+  <dt>前回ログイン</dt>
+  <dd id="last-logined-at">
+  ]]
+ngx.print ngx.var.cookie_last_created_at
+ngx.print [[
+</dd>
+  <dt>最終ログインIPアドレス</dt>
+  <dd id="last-logined-ip">]]
+ngx.print ngx.var.cookie_last_ip
+ngx.print [[
+</dd>
+</dl>
+
+<div class="panel panel-default">
+  <div class="panel-heading">
+    お客様ご契約ID：]]
+ngx.print ngx.var.cookie_login
+ngx.print [[ 様の代表口座
+  </div>
+  <div class="panel-body">
+    <div class="row">
+      <div class="col-sm-4">
+        普通預金<br>
+        <small>東京支店　1111111111</small><br>
+      </div>
+      <div class="col-sm-4">
+        <p id="zandaka" class="text-right">
+          ―――円
+        </p>
+      </div>
+
+      <div class="col-sm-4">
+        <p>
+          <a class="btn btn-success btn-block">入出金明細を表示</a>
+          <a class="btn btn-default btn-block">振込・振替はこちらから</a>
+        </p>
+      </div>
+
+      <div class="col-sm-12">
+        <a class="btn btn-link btn-block">定期預金・住宅ローンのお申込みはこちら</a>
+      </div>
+    </div>
+  </div>
+</div>
+    </div>
+  </body>
+</html>
+]]
