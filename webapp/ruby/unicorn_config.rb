6b96a94b00e903ee6484edaf336c67e5d41b7787
Kazuki Yamaguchi <k@rhe.jp>
2015-09-26 11:47:19 +0900

    fix

--- webapp/ruby/unicorn_config.rb
+++ webapp/ruby/unicorn_config.rb
@@ -4,5 +4,5 @@ if ENV["USER"] == "isucon"
   pid "/home/isucon/webapp/ruby/unicorn.pid"
   listen "/sock/unicorn.sock"
 else
-  listen "/sock/#{ENV["HOME"]}.sock"
+  listen "/sock/#{ENV["USER"]}.sock"
 end
