06741d18ffcc37c850095c64898396352f4eec4e
Kazuki Yamaguchi <k@rhe.jp>
2015-09-26 15:24:20 +0900

    u

--- webapp/ruby/app.rb
+++ webapp/ruby/app.rb
@@ -205,7 +205,7 @@ SQL
     end
 
     comments_of_friends = db.xquery(
-      'SELECT * FROM comments WHERE user_id IN (?) ORDER BY created_at DESC LIMIT 1000', fids.join(",")).each do |comment|
+      'SELECT * FROM comments WHERE user_id IN (?) ORDER BY created_at DESC LIMIT 10', fids.join(",")).each do |comment|
       entry = db.xquery('SELECT * FROM entries WHERE id = ?', comment[:entry_id]).first
       entry[:is_private] = (entry[:private] == 1)
       #next if entry[:is_private] && !permitted?(entry[:user_id])
